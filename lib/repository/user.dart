ea42ab0613cd01a8c4ce5b7c36988705bf00aa02
Ritika Guleria <ritikaguleria95@gmail.com>
2023-08-22 18:54:35 +0530

    added freezed class successfully

---
+++ lib/repository/user.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'user.freezed.dart';
+
+part 'user.g.dart';
+
+@freezed
+class User with _$User {
+  const factory User({
+    required String email,
+    @JsonKey(name: "first_name") required String firstName,
+    @JsonKey(name: "last_name") required String lastName,
+    required String avatar,
+  }) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
